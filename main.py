3b414a59abf41b4bdc980ebe81b7021c211550b6
creeper-0910 <56744841+creeper-0910@users.noreply.github.com>
2024-05-06 13:00:13 +0900

    タイムスタンプ保存されないの知らなかった...

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import os
 
 files = os.listdir("./inagi/")
 files = [os.path.join("./inagi/", f) for f in files]
-files.sort(key=os.path.getctime, reverse=True)
+files.sort(key=os.path.getctime, reverse=False)
 result = [file[1:] for file in files]
 with open('data.txt', 'w', encoding='utf-8') as f:
     f.write("\n".join(result))
