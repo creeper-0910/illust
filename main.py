6da3f25b564001687a0fceeb6ceaf435bb966337
creeper-0910 <56744841+creeper-0910@users.noreply.github.com>
2024-05-06 12:24:48 +0900

    second commit

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-import csv
 import os
 
 files = os.listdir("./inagi/")
 files = [os.path.join("./inagi/", f) for f in files]
 files.sort(key=os.path.getmtime, reverse=True)
 result = [file[1:] for file in files]
-with open('data.csv', 'w', encoding='utf-8') as f:
-    writer = csv.writer(f)
-    writer.writerow(result)
+with open('data.txt', 'w', encoding='utf-8') as f:
+    f.write("\n".join(result))
